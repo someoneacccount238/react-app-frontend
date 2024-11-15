185359dab3212e3b37fefc38c633191abf90d931
someoneacccount238 <someoneacccount238@gmail.com>
2024-11-15 15:29:12 +0200

    Updates

--- client/src/components/SearchableDropdown/useForm.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export const useForm = (initialValues) => {
-  const [values, setValues] = useState(initialValues);
-  const handleChange = (e) =>
-    setValues({ ...values, [e.target.name]: e.target.value });
-
-  return [values, handleChange];
-};
